92eda337b8973dfd54df1a6faf050ec001fc0ca3
Adriana Gonzalez Palomo <adrianag@c3r8s6.42malaga.com>
2026-03-26 17:17:44 +0100

    trying push swap with inversion coefficient to decide movements

--- new_push_swap.c
+++ new_push_swap.c
@@ -103,86 +103,93 @@ void    swap(t_list **list)
 void    sa(t_list **a)
 {
     swap(a);
-    write(1, "sa\n", 3);
+    /*write(1, "sa\n", 3);*/
 }
 
 void    sb(t_list **b)
 {
     swap(b);
-    write(1, "sb\n", 3);
+    /*write(1, "sb\n", 3);*/
 }
 
 void    ss(t_list **a, t_list **b)
 {
     swap(a);
     swap(b);
-    write(1, "ss\n", 3);
+    /*write(1, "ss\n", 3);*/
 }
 
 void    push(t_list **a, t_list **b)
 {
     t_list  *node;
-    if (!(*b) || !b)
+    if (!b || !(*b))
     {
         return ;
     }
     node = *b;
+    if (node->next == node)
+    {
+        *b = NULL;
+        node->next = node->prev = node; 
+        ft_lstadd_front(a, node);
+        return ;
+    }
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    *b = node->next;
 
-    (*b)->prev->next = (*b)->next;
-    (*b)->next->prev = (*b)->prev;
-    *b = (*b)->next;
-
+    node->next = node->prev = node;
     ft_lstadd_front(a, node);
 }
 
 void    pa(t_list **a, t_list **b)
 {
     push(a, b);
-    write(1, "pa\n", 3);
+    /*write(1, "pa\n", 3);*/
 }
 
 void    pb(t_list **a, t_list **b)
 {
     push(b, a);
-    write(1, "pb\n", 3);
+    /*write(1, "pb\n", 3);*/
 }
 
 void    ra(t_list **a)
 {
     *a = (*a)->next;    
-    write(1, "ra\n", 3);
+    /*write(1, "ra\n", 3);*/
 }
 
 void    rb(t_list **b)
 {
     *b = (*b)->next;    
-    write(1, "rb\n", 3);
+    /*write(1, "rb\n", 3);*/
 }
 
 void    rr(t_list **a, t_list **b)
 {
     *a = (*a)->next; 
     *b = (*b)->next;    
-    write(1, "rr\n", 3);
+    /*write(1, "rr\n", 3);*/
 }
 
 void    rra(t_list **a)
 {
     *a = (*a)->prev;    
-    write(1, "rra\n", 4);
+    /*write(1, "rra\n", 4);*/
 }
 
 void    rrb(t_list **b)
 {
     *b = (*b)->prev;    
-    write(1, "rrb\n", 4);
+    /*write(1, "rrb\n", 4);*/
 }
 
 void    rrr(t_list **a, t_list **b)
 {
     *a = (*a)->prev; 
     *b = (*b)->prev;    
-    write(1, "rrr\n", 4);
+    /*write(1, "rrr\n", 4);*/
 }
 
 double compute_disorder(t_list **a)
@@ -205,9 +212,9 @@ double compute_disorder(t_list **a)
             mistakes += 1;
         }
         temp = temp->next;
-    }    
-    printf("mistakes: %d\n", mistakes);
-    printf("pairs: %d\n", total_pairs);
+    }
+    /*printf("mistakes: %d\n", mistakes);
+    printf("pairs: %d\n", total_pairs);*/
     return (1 - (double)mistakes / (double)total_pairs);
 }
 
@@ -219,9 +226,9 @@ int check_valid(char **argv, int argc)
     char ch;
 
     index = 0;
-    j = 0;
-    while (index < argc - 1)
+    while (index < argc)
     {
+        j = 0;
         while ((ch = argv[index + 1][j]) != '\0')
         {
             if (!(ch >= '0' && ch <= '9') && ch != ' ')
@@ -233,16 +240,15 @@ int check_valid(char **argv, int argc)
     return (TRUE); 
 }
 
-t_list  *load_list(char **argv, int argc)
+void    load_list(t_list **list, char **argv, int argc)
 {
     int index;
     int j;
     int flag;
     t_list  *node = NULL;
-    t_list  *list = NULL;
 
     index = 0;
-    while (index < argc - 1)
+    while (index < argc)
     {
         j = 0;
         flag = 0;
@@ -253,7 +259,7 @@ t_list  *load_list(char **argv, int argc)
                 || argv[index + 1][j] == '-') && flag == 0)
             {
                 node = ft_create_node(ft_atoi(&argv[index + 1][j]));
-                ft_lstadd_back(&list, node);
+                ft_lstadd_back(list, node);
                 flag = 1;
             }
             else if (argv[index + 1][j] == ' ')
@@ -262,31 +268,127 @@ t_list  *load_list(char **argv, int argc)
         }
         index++;
     }
-    return (list); 
 }
 
-t_bool check_duplicates(t_list *list)
+/* Calculates duplicates and inversions at the same time */
+int check_duplicates(t_list *list)
 {
     t_list  *current;
     t_list  *temp;
+    int inv;
 
     current = list;
     temp = list->next;
+    inv = 0;
     while (current->next != list)
     {
         while (temp != list)
         {
-            printf("%dy%d:", current->number, temp->number);
+            /*printf("%dy%d:", current->number, temp->number);*/
             if (current->number == temp->number) 
             {
-                return (FALSE);
+                return (-1);
+            }  
+            else if (current->number > temp->number) 
+            {
+                inv++;
             }   
             temp = temp->next;
         }
         current = current->next;
         temp = current->next;
     }
-    return (TRUE);
+    return (inv);
+}
+
+int *check_delta(t_list **a, t_list **b, int *mov)
+{
+    sa(a);
+    mov[0] = check_duplicates(*a);
+    sa(a);
+
+    if (*b)
+    {   
+        pa(a, b);
+        mov[1] = check_duplicates(*a);
+        pb(a, b);
+    }
+    /* Pasar primer numero de a a b */
+    pb(a, b);
+    mov[2] = check_duplicates(*a);
+    pa(a, b);
+
+    ra(a);
+    mov[3] = check_duplicates(*a);
+    rra(a);
+
+    rra(a);
+    mov[4] = check_duplicates(*a);
+    ra(a);
+    return (mov);
+}
+
+int best_movement(int *movements)
+{
+    int min;
+    int index;
+
+    index = 0;
+    min = 0;
+    while (index < 6)
+    {
+        if (movements[index] < movements[min])
+            min = index;
+        index++;
+    }
+    return (min);
+}
+
+void    perform_movement(t_list **a, t_list **b)
+{
+    int best_mov;
+    int mov[6];
+
+    best_mov = 0;
+    mov[5] = check_duplicates(*a);
+    best_mov = best_movement(check_delta(a, b, mov));
+    if (best_mov == 0)
+    {
+        sa(a);
+        write(1, "sa\n", 3);
+    }    
+    else if (best_mov == 1)
+    {
+        pa(a, b);
+        write(1, "pa\n", 3);
+    }  
+    else if (best_mov == 2)
+    {
+        pb(a, b);
+        write(1, "pb\n", 3);
+    }
+    else if (best_mov == 3)
+    {
+        ra(a);
+        write(1, "ra\n", 3);
+    }
+    else if (best_mov == 4)
+    {
+        rra(a);
+        write(1, "rra\n", 4);
+    }
+}
+
+void    order_list(t_list **a, t_list **b)
+{
+    t_list  *temp;
+
+    temp = *a;
+    while (temp->next != *a)
+    {
+        perform_movement(a, b);
+        temp = temp->next;
+    }
 }
 
 int main(int argc, char **argv)
@@ -296,45 +398,45 @@ int main(int argc, char **argv)
     t_list  *list2 = NULL;
     t_list  *list3 = NULL;
     t_list  *node;
+    int disorder = 0;
 
     index = 0;
     if (argc < 2)
     {
-        printf("Error");
-        return (1);
+        char *debug_argv[] = {"push_swap", "4 3 2 6", "5"};
+        argc = 2;
+        argv = debug_argv;
     }
-    list = load_list(argv, argc);
-    
+    if (check_valid(argv, argc) == TRUE)
+    {
+        load_list(&list, argv, argc);
+    }
+    if ((disorder = check_duplicates(list)) == TRUE)
+    {
+        printf("Error");
+        /*return (1);*/
+        /* free list */
+    }
+    printf("This is chaos:%d\n", disorder);
+    /*int valid = check_valid(argv, argc);
+    printf("%d\n", valid);*/
+
     printf("%d\n", list->number);
     printf("%d\n", list->next->number);
     printf("%d\n", list->next->next->number);
- 
-    double desorden = compute_disorder(&list);
-    printf("%f\n", desorden);
+    printf("%d\n", list->next->next->next->number);
+    printf("%d\n", list->next->next->next->next->number);
 
-    int valid = check_valid(argv, argc);
-    printf("%d\n", valid);
+    /*int check = check_duplicates(list);
+    printf("Duplicates:%d\n", check);
+    printf("Inversions:%d\n", inv);*/
 
-    if (check_valid(argv, argc) == TRUE)
-    {
-        list3 = load_list(argv, argc);
-    }    
-    if (check_duplicates(list3) == FALSE)
-    {
-
-    }
-    printf("%d\n", list3->number);
-    printf("%d\n", list3->next->number);
-    printf("%d\n", list3->next->next->number);
-    printf("%d\n", list3->next->next->next->number);
-    printf("%d\n", list3->next->next->next->next->number);
-
-    int check = check_duplicates(list3);
-    printf("%d\n", check);
-
-
-    int atoi = ft_atoi("3465237636732");
-    printf("%d", atoi);
+    order_list(&list, &list3);
+    printf("New list:\n%d\n", list->number);
+    printf("%d\n", list->next->number);
+    printf("%d\n", list->next->next->number);
+    printf("%d\n", list->next->next->next->number);
+    printf("%d\n", list->next->next->next->next->number);
 
     return (0);
 }
