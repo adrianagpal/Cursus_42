0b7378a557e039084caf591607c8470c5b9d4cf2
adrianagpal <adriana@DESKTOP-1AIC7CD.localdomain>
2026-05-06 01:23:55 +0200

    python module 7

--- python/module_07/battle.py
+++ python/module_07/battle.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python3
 from ex0 import factory
 
-print("Testing factory")
-flame = factory.FlameFactory()
-flame.create_base()
-flame.create_evolved()
-print()
-print("Testing factory")
-aqua = factory.AquaFactory()
-aqua.create_base()
-aqua.create_evolved()
-print()
+def verify_factory(factory: factory.CreatureFactory):
+    
+    print("Testing factory")
+    base = factory.create_base()
+    print(base.describe())
+    print(base.attack())
+
+    evolution = factory.create_evolved()
+    print(evolution.describe())
+    print(evolution.attack())
+
+
+def creature_battle(factory1: factory.CreatureFactory, factory2: factory.CreatureFactory):
+    print("Testing battle")
+    base1 = factory1.create_base()
+    print(base1.describe())
+    print("vs.")
+    base2 = factory2.create_base()
+    print(base2.describe())
+    print("fight!")
+    print(base1.attack())
+    print(base2.attack())
+
+def main():
+    flame_factory = factory.FlameFactory()
+    verify_factory(flame_factory)
+    print()
+
+    aqua_factory = factory.AquaFactory()
+    verify_factory(aqua_factory)
+    print()
+
+    creature_battle(flame_factory, aqua_factory)
+
+main()
---
+++ python/module_07/ex0/creature.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+class Creature(ABC):
+    def __init__(self):
+        self.name: str = self.__class__.__name__
+        self.creature_type: str
+
+    @abstractmethod
+    def attack(self) -> str:
+        pass
+
+    def describe(self) -> str:
+        return f"{self.name} is of type {self.creature_type}"
+
+class Flameling(Creature):
+    def __init__(self):
+        super().__init__()
+        self.creature_type = "Fire"
+
+    def attack(self) -> str:
+        return f"{self.name} uses Ember!"
+
+
+class Pyrodon(Creature):
+    def __init__(self):
+        super().__init__()
+        self.creature_type = "Fire/Flying"
+
+    def attack(self) -> str:
+        return f"{self.name} uses Flamethrower!"
+
+
+class Aquabub(Creature):
+    def __init__(self):
+        super().__init__()
+        self.creature_type = "Water"
+
+    def attack(self) -> str:
+        return f"{self.name} uses Water Gun!"
+
+
+class Torragon(Creature):
+    def __init__(self):
+        super().__init__()
+        self.creature_type = "Water"
+
+    def attack(self) -> str:
+        return f"{self.name} uses Hydro Pump!"
